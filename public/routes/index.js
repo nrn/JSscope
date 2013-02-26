f0d8d5cec3c6360b7be943bed161bf1c1fe0aea1
Nick Niemeir <nick.niemeir@gmail.com>
2013-02-25 23:30:16 -0700

    Example

--- public/routes/index.js
+++ public/routes/index.js
@@ -6,39 +6,56 @@ var f = require('flates')
     ]
 
 module.exports = function () {
-  return f.d() + scripts.map(function (script) {
-    return f.script({ src: script })
-  }).join('') +
-  f.div(
-    [ f.h1('JS scope')
-    , 'An attempt to explain lexical, block level scope.'
-    , f.aside({ class: 'warning'},
-        [ 'This website is serving as a playground for some odd technology'
-        , 'with the intention that it will one day be suitable for the purpose'
-        , 'of learning about scope in JavaScript.'
-        ].join(' ')
-      )
-    , f.div(
-      [ "A quick overview of scope in JavaScript, we'll look at this all in"
-      , 'detail in a second:'
-      , 'Any variable available in the scope in which a function is declared'
-      , 'is available inside that function. Declaring something new in a scope'
-      , 'shadows any outer uses of the same name, rendering them unreachable.'
-      , 'Functions are the only unit of scope, other blocks are part of the'
-      , 'function they are contained within.'
-      , 'All arguments to a function are declared inside that scope'
-      , 'if they are set or not. Along with the magical "this" and'
-      , '"arguments" variables.'
-      ].join(' '))
-    , f.div(
-      [ 'One way to thing about scope is as a series of concentric circles'
-      , 'with the inner circles able to acces the variables in all outer'
-      , 'circles, but outer circles not able to get to the inner ones.'
-      , 'These outer circles are always availible whenever the function'
-      , 'is invoked, even if it has been returned out of the scope it'
-      , 'was defined in. This is know as closure, and it allows many of'
-      , 'the poweful abstractions used in JavaScript.'
-      ].join(' '))
-    ]
-  )
+  return f.d() + f.link({href: '/style.css', rel: 'stylesheet' }) +
+    f.div(
+      [ f.h1('JS scope')
+      , 'An attempt to explain lexical, block level scope.'
+      , f.aside({ class: 'warning'},
+          [ 'This website is serving as a playground for some odd technology'
+          , 'with the intention that it will one day be suitable for the purpose'
+          , 'of learning about scope in JavaScript.'
+          ].join(' ')
+        )
+      , f.p(
+        [ "A quick overview of scope in JavaScript, we'll look at this all in"
+        , 'detail in a second:'
+        , 'Any variable available in the scope in which a function is declared'
+        , 'is available inside that function. Declaring something new in a scope'
+        , 'shadows any outer uses of the same name, rendering them unreachable.'
+        , 'Functions are the only unit of scope, other blocks are part of the'
+        , 'function they are contained within.'
+        , 'All arguments to a function are declared inside that scope'
+        , 'if they are set or not. Along with the magical "this" and'
+        , '"arguments" variables.'
+        ].join(' '))
+      , f.p(
+        [ 'One way to think about scope is as a series of concentric circles'
+        , 'with the inner circles able to acces the variables in all outer'
+        , 'circles, but outer circles not able to get to the inner ones.'
+        , 'These outer circles are always availible whenever the function'
+        , 'is invoked, even if it has been returned out of the scope it'
+        , 'was defined in. This is know as closure, and it allows many of'
+        , 'the poweful abstractions used in JavaScript.'
+        ].join(' '))
+      , f.p('Insert demo here')
+      , f.p(
+        [ 'The inner function can always acces the outer scope:'
+        , f.pre({ class: 'example'},
+          [ 'var a = 0'
+          , 'function inner () {'
+          , '  a += 1'
+          , '}'
+          , ''
+          , 'inner()'
+          , 'console.log(a) // 1'
+          , 'inner()'
+          , 'console.log(a) // 2'
+          , 'inner()'
+          , 'console.log(a) // 3'
+          ].join('\n'))
+        ].join(' '))
+      ]
+    ) + scripts.map(function (script) {
+      return f.script({ src: script })
+    }).join('')
 }
